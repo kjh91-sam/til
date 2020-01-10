ed47b6d0503dd2632bfb974ff60be06b4a1ac9ff
Sam Juhyun Kang <55772399+kjh91-sam@users.noreply.github.com>
2020-01-10 18:59:33 +0900

    Update 2016LeapYear.c
    문자열로 배열을 만들었을 때 포인터 사용을 연습

--- 2016LeapYear.c
+++ 2016LeapYear.c
@@ -6,7 +6,7 @@ char* solution(int a, int b) {
     char* answer = (char*)malloc(4);
     
     int days[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    char dayWeek[7][4] = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+    char* dayWeek[7] = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
     int daysSum = 0;
     
     for(int i = 0; i < a-1; i++) {
@@ -14,7 +14,7 @@ char* solution(int a, int b) {
     }
     
     daysSum += b;
-
-    sprintf(answer, "%s", dayWeek[daysSum % 7]);
+    
+    answer = dayWeek[daysSum % 7];
     return answer;
 }
