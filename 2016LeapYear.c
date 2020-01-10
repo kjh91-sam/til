020ab2c7b69b258056168e66dfa436ed1448a92a
Sam Juhyun Kang <55772399+kjh91-sam@users.noreply.github.com>
2020-01-10 18:58:09 +0900

    Update and rename 2016LeapYear.js to 2016LeapYear.c
    C언어로 다시 구현해봄

---
+++ 2016LeapYear.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+char* solution(int a, int b) {
+    char* answer = (char*)malloc(4);
+    
+    int days[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    char dayWeek[7][4] = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+    int daysSum = 0;
+    
+    for(int i = 0; i < a-1; i++) {
+        daysSum += days[i];
+    }
+    
+    daysSum += b;
+
+    sprintf(answer, "%s", dayWeek[daysSum % 7]);
+    return answer;
+}
