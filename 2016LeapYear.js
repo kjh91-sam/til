46e2aeb3400bd46b7107c8e858bc28895c48c0c9
Sam Juhyun Kang <55772399+kjh91-sam@users.noreply.github.com>
2020-01-10 18:05:57 +0900

    Update 2016LeapYear.js
    요일을 나열한 배열을 만들어서 switch문을 없앰

--- 2016LeapYear.js
+++ 2016LeapYear.js
@@ -1,6 +1,6 @@
 function solution(a, b) {
-    let answer = '';
     let days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let dayWeek = ['THU', 'FRI', 'SAT', 'SUM', 'MON', 'TUE', 'WED'];
     let daysSum = 0;
     
     for(let i = 0; i < a-1; i++) {
@@ -8,34 +8,6 @@ function solution(a, b) {
     }
     
     daysSum += b;
-    
-    let dayWeek = daysSum % 7;
-    
-    switch(dayWeek) {
-        case 0 : 
-            answer = 'THU';
-            break;
-        case 1 :
-            answer = 'FRI';
-            break;
-        case 2 :
-            answer = 'SAT';
-            break;
-        case 3 :
-            answer = 'SUN';
-            break;
-        case 4 :
-            answer = 'MON';
-            break;
-        case 5 :
-            answer = 'TUE';
-            break;
-        case 6 :
-            answer = 'WED';
-            break;
-        dafault :
-            break;
-    }
 
-    return answer;
+    return dayWeek[(daysSum % 7)];
 }
