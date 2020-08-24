8882016d5927d35790636a54705811772655b228
Sam Juhyun Kang <kjh91.sam@gmail.com>
2020-08-24 19:34:51 +0900

    started to make set making function in c

--- 2_leetcode_algoexpert/3sum.c
+++ 2_leetcode_algoexpert/3sum.c
@@ -2,28 +2,36 @@
 #include <stdio.h>
 
 int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes){
-    int **returnArray;
-    returnArray = (int **)malloc(sizeof(int *) * (*returnSize + 1));
-    int ri;
-    for (ri = 0; ri < *returnSize; ri++)
-        returnArray[ri] = (int *)malloc(sizeof(int) * 3);
-    returnColumnSizes = (int **)malloc(sizeof(int) * (*returnSize + 1));
-    ri = 0;
-    for (int i = 0; i < numsSize - 2; i++) {
-        for (int j = 1; j < numsSize - 1; j++) {
-            for (int k = 2; k < numsSize; k++) {
-                if ((nums[i] + nums[j] + nums[k]) == 0) {
-                    //returnArray[ri] = {nums[i], nums[j], nums[k]};
-                    returnArray[ri][0] = nums[i];
-                    returnArray[ri][1] = nums[j];
-                    returnArray[ri][2] = nums[k];
-                    (*returnColumnSizes)[ri] = sizeof(returnArray[ri]);
-                    ri++;
-                }
-            }
-        }
-    }
-    return returnArray;
+	int** retArr;
+	int	temp[numsSize];
+	int	target = 0;
+	int	cases = 0;
+
+	for (int n = 0; n < numsSize; n++) {
+		for (int m = 0; ; m < sizeof(temp) / sizeof(int); m++) {
+			if (nums[n] ==
+		}
+	}
+
+	for (int i = 0; i < numsSize - 2; i++)
+		for (int j = i + 1; j  < numsSize - 1; j++)
+			for (int k = j + 1; k < numsSize; k++)
+				if ((nums[i] + nums[j] + nums[k]) == target) {
+					cases++;
+					if (cases == 1)
+						retArr = (int **)malloc(sizeof(int*) * cases);
+					else
+						retArr = (int **)realloc(retArr, sizeof(int*) * cases);
+					printf("allocation has successed with i : %d, j : %d, k : %d\n", i, j, k);
+					retArr[cases - 1] = (int *)malloc(sizeof(int) * 3);
+					retArr[cases - 1][0] = nums[i];
+					retArr[cases - 1][1] = nums[j];
+					retArr[cases - 1][2] = nums[k];
+					*returnColumnSizes = (int *)realloc(*returnColumnSizes, sizeof(int) * cases);
+					(*returnColumnSizes)[cases - 1] = 3;
+				}
+	*returnSize = cases;
+	return retArr;
 }
 
 int	main() {
